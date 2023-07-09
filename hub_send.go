21537aceb4881fc0bf5d1431710dc6da2125d8bc
Jason Frame <jwf@jasonframe.co.uk>
2023-07-09 12:29:42 +0100

    Rework send methods to use sendContext2

--- hub_send.go
+++ hub_send.go
@@ -7,17 +7,10 @@ func (s *Hub[ID, IM]) SendToConnection(conn *Conn[ID, IM], msg any) error {
 }
 
 func (s *Hub[ID, IM]) SendToConnectionContext(ctx context.Context, conn *Conn[ID, IM], msg any) error {
-	select {
-	case s.outgoingInt <- connectionOutgoingMessage[ID, IM]{
+	return sendContext2[any](ctx, s.context, s.outgoingInt, connectionOutgoingMessage[ID, IM]{
 		Connection: conn,
 		Msg:        msg,
-	}:
-		return nil
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-s.context.Done():
-		return s.context.Err()
-	}
+	})
 }
 
 func (s *Hub[ID, IM]) SendToConnections(conns []*Conn[ID, IM], msg any) error {
@@ -25,17 +18,10 @@ func (s *Hub[ID, IM]) SendToConnections(conns []*Conn[ID, IM], msg any) error {
 }
 
 func (s *Hub[ID, IM]) SendToConnectionsContext(ctx context.Context, conns []*Conn[ID, IM], msg any) error {
-	select {
-	case s.outgoingInt <- multiConnectionOutgoingMessage[ID, IM]{
+	return sendContext2[any](ctx, s.context, s.outgoingInt, multiConnectionOutgoingMessage[ID, IM]{
 		Connections: conns,
 		Msg:         msg,
-	}:
-		return nil
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-s.context.Done():
-		return s.context.Err()
-	}
+	})
 }
 
 func (s *Hub[ID, IM]) SendToClient(clientID ID, msg any) error {
@@ -43,17 +29,10 @@ func (s *Hub[ID, IM]) SendToClient(clientID ID, msg any) error {
 }
 
 func (s *Hub[ID, IM]) SendToClientContext(ctx context.Context, clientID ID, msg any) error {
-	select {
-	case s.outgoingInt <- clientOutgoingMessage[ID]{
+	return sendContext2[any](ctx, s.context, s.outgoingInt, clientOutgoingMessage[ID]{
 		ClientID: clientID,
 		Msg:      msg,
-	}:
-		return nil
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-s.context.Done():
-		return s.context.Err()
-	}
+	})
 }
 
 func (s *Hub[ID, IM]) SendToClients(clientIDs []ID, msg any) error {
@@ -61,15 +40,8 @@ func (s *Hub[ID, IM]) SendToClients(clientIDs []ID, msg any) error {
 }
 
 func (s *Hub[ID, IM]) SendToClientsContext(ctx context.Context, clientIDs []ID, msg any) error {
-	select {
-	case s.outgoingInt <- multiClientOutgoingMessage[ID]{
+	return sendContext2[any](ctx, s.context, s.outgoingInt, multiClientOutgoingMessage[ID]{
 		ClientIDs: clientIDs,
 		Msg:       msg,
-	}:
-		return nil
-	case <-ctx.Done():
-		return ctx.Err()
-	case <-s.context.Done():
-		return s.context.Err()
-	}
+	})
 }
