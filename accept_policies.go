06893e8b3ec54a230b6ca4dbb75afdc41cca080e
Jason Frame <jwf@jasonframe.co.uk>
2023-08-04 09:45:11 +0100

    Document connect policy factories

--- accept_policies.go
+++ accept_policies.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+// FirstWins creates a policy that permit a single connection per unique client ID.
+// Any additional connections with the same client ID will be immediately disconnected.
 func FirstWins[ID comparable, IM any]() func(conn *Conn[ID, IM], roster *Roster[ID, IM]) ([]*Conn[ID, IM], error) {
 	return func(conn *Conn[ID, IM], roster *Roster[ID, IM]) ([]*Conn[ID, IM], error) {
 		if len(roster.ClientConnections(conn.ClientID())) > 0 {
@@ -13,12 +15,17 @@ func FirstWins[ID comparable, IM any]() func(conn *Conn[ID, IM], roster *Roster[
 	}
 }
 
+// LastWins creates a policy that permit a single connection per unique client ID.
+// Each additional connection with the same client ID will cause its predecessor to
+// be disconnected.
 func LastWins[ID comparable, IM any]() func(conn *Conn[ID, IM], roster *Roster[ID, IM]) ([]*Conn[ID, IM], error) {
 	return func(conn *Conn[ID, IM], roster *Roster[ID, IM]) ([]*Conn[ID, IM], error) {
 		return roster.ClientConnections(conn.ClientID()), nil
 	}
 }
 
+// MultipleClientConnectionsAllowed creates a policy that permits multiple simultaneous
+// connections per unique client ID.
 func MultipleClientConnectionsAllowed[ID comparable, IM any]() func(conn *Conn[ID, IM], roster *Roster[ID, IM]) ([]*Conn[ID, IM], error) {
 	return func(conn *Conn[ID, IM], roster *Roster[ID, IM]) ([]*Conn[ID, IM], error) {
 		return nil, nil
